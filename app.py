9ec8ebcc5396e0185ef88f70acc057975db6f68d
moulyar05 <moulyar05@gmail.com>
2026-03-24 09:52:35 +0530

    Added SQLite database to save user registration

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
+
+def init_db():
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            email TEXT,
+            age INTEGER,
+            weight REAL,
+            goal TEXT,
+            level TEXT
+        )
+    """)
+    conn.commit()
+    conn.close()
 
 @app.route("/")
 def home():
@@ -15,9 +33,19 @@ def register():
         weight = request.form["weight"]
         goal = request.form["goal"]
         level = request.form["level"]
-        print(f"New user: {name}, {email}, {age}kg, Goal: {goal}, Level: {level}")
-        return f"Welcome {name}! Profile created successfully! ✅"
+
+        conn = sqlite3.connect("users.db")
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO users (name, email, age, weight, goal, level) VALUES (?, ?, ?, ?, ?, ?)",
+                       (name, email, age, weight, goal, level))
+        conn.commit()
+        conn.close()
+
+        return f"Welcome {name}! Profile saved successfully! ✅"
     return render_template("register.html")
 
 if __name__ == "__main__":
+    init_db()
     app.run(debug=True)
+
+
